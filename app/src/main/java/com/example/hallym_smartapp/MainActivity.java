d16f178579a549af039544d77fa6de3e1b436c9d
MminJJ <alsejd107@naver.com>
2022-10-24 12:11:43 +0900

    예약 화면 수정

--- app/src/main/java/com/example/hallym_smartapp/MainActivity.java
+++ app/src/main/java/com/example/hallym_smartapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hallym_smartapp.Reservation.Activity.FiveFloorActivity;
+import com.example.hallym_smartapp.Reservation.Activity.FourFloorActivity;
+import com.example.hallym_smartapp.Reservation.Activity.ThirdFloorActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -17,6 +19,35 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(new Intent(this, Splash.class));
 
+        Button btn5 = (Button) findViewById(R.id.fiveFloor);
+        btn5.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), FiveFloorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn4 = (Button) findViewById(R.id.fourFloor);
+        btn4.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), FourFloorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn3 = (Button) findViewById(R.id.thirdFloor);
+        btn3.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), ThirdFloorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 

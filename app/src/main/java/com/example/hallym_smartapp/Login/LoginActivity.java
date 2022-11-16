941aaa0a1d1a4778dc0fab3de0b38b057ff9d1dd
leechaeeyoung <zs1397@naver.com>
2022-11-16 18:12:49 +0900

    좌석 설정

--- app/src/main/java/com/example/hallym_smartapp/Login/LoginActivity.java
+++ app/src/main/java/com/example/hallym_smartapp/Login/LoginActivity.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.hallym_smartapp.MainActivity;
 import com.example.hallym_smartapp.R;
 import com.example.hallym_smartapp.Reservation.Activity.ReservationActivity;
+import com.example.hallym_smartapp.Reservation.Function.ReserveDialog;
 import com.example.hallym_smartapp.Splash;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,14 +26,14 @@ import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
     EditText idText, passwordText;
-    Button loginBt,signBt;
+    Button loginBt, signBt, thirdFloor;
     Intent intent;
 
     private final FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private final DatabaseReference databaseReference = firebaseDatabase.getReference();
 
     public static boolean loginStatus = false; // 기본으로 로그인 상태가 아님으로 표시
-    public static String loginId="";
+    public static String loginId = "";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +49,13 @@ public class LoginActivity extends AppCompatActivity {
         passwordText = findViewById(R.id.passwordText);
         loginBt = findViewById(R.id.loginBt);
         signBt = findViewById(R.id.signBt);
+        thirdFloor=findViewById(R.id.thirdFloor);
 
+        // test
+        thirdFloor.setOnClickListener(v -> {
+            intent = new Intent(getApplicationContext(), ReserveDialog.class);
+            startActivity(intent);
+        });
         // 회원가입 버튼 클릭 이밴트
         signBt.setOnClickListener(v -> {
             intent = new Intent(getApplicationContext(), SignUp.class);
@@ -78,45 +85,36 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(intent);
         });*/
     }
-        // 등록된 유저인지 확인
-        public void loginCheck(final String id,final String pwd){
-            final Query query = databaseReference.child("User");
-            query.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if(snapshot.hasChild(id))
-                        if(snapshot.child(id).child("pwd").getValue().equals(pwd)){
-                            Log.e("loginCheck: ", "로그인 되었습니다.");
-                            Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
-                            loginStatus = true;
-                            loginId = id;
 
-                            intent = new Intent(getApplicationContext(), MainActivity.class);
-                            startActivity(intent);
-                        } else{
-                            Log.e("loginCheck: ","비밀번호가 틀렸습니다.");
-                            Toast.makeText(getApplicationContext(), "아이디 또는 패스워드를 다시 확인하세요.", Toast.LENGTH_LONG).show();
-                    }else{
-                        Log.e("loginCheck: "," 해당 아이디가 존재하지 않습니다.");
+    // 등록된 유저인지 확인
+    public void loginCheck(final String id, final String pwd) {
+        final Query query = databaseReference.child("User");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.hasChild(id))
+                    if (snapshot.child(id).child("pwd").getValue().equals(pwd)) {
+                        Log.e("loginCheck: ", "로그인 되었습니다.");
+                        Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
+                        loginStatus = true;
+                        loginId = id;
+
+                        intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Log.e("loginCheck: ", "비밀번호가 틀렸습니다.");
                         Toast.makeText(getApplicationContext(), "아이디 또는 패스워드를 다시 확인하세요.", Toast.LENGTH_LONG).show();
                     }
+                else {
+                    Log.e("loginCheck: ", " 해당 아이디가 존재하지 않습니다.");
+                    Toast.makeText(getApplicationContext(), "아이디 또는 패스워드를 다시 확인하세요.", Toast.LENGTH_LONG).show();
                 }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-                    Log.e("loadUser:onCancelled", String.valueOf(error.toException()));
-                }
-            });
-        }
-
-    // TEST code
-/*        Button btn3 = (Button) findViewById(R.id.thirdFloor);
-        btn3.setOnClickListener(new View.OnClickListener(){
+            }
 
             @Override
-            public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), ThirdFloorActivity.class);
-                startActivity(intent);
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("loadUser:onCancelled", String.valueOf(error.toException()));
             }
-        });*/
+        });
+    }
 }

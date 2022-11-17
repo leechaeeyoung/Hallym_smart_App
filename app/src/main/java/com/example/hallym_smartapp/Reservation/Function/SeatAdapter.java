23757930762e60d5c49ae9834ab7ee4ca2a49c39
leechaeeyoung <zs1397@naver.com>
2022-11-17 23:25:26 +0900

    프래그수정

--- app/src/main/java/com/example/hallym_smartapp/Reservation/Function/SeatAdapter.java
+++ app/src/main/java/com/example/hallym_smartapp/Reservation/Function/SeatAdapter.java
@@ -1,9 +1,17 @@
 package com.example.hallym_smartapp.Reservation.Function;
 
+import static com.example.hallym_smartapp.Login.LoginActivity.loginId;
+import static com.example.hallym_smartapp.Login.LoginActivity.loginStatus;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatTextView;
@@ -13,6 +21,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hallym_smartapp.Login.UserDTO;
 import com.example.hallym_smartapp.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -24,6 +38,9 @@ public class SeatAdapter extends ListAdapter<String, SeatAdapter.MyViewHolder> {
     ReserveDialog room1 = new ReserveDialog();
 
     final int floorNum = 3;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
     public SeatAdapter(@NonNull DiffUtil.ItemCallback<String> diffCallback) {
         super(diffCallback);
@@ -37,31 +54,73 @@ public class SeatAdapter extends ListAdapter<String, SeatAdapter.MyViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
         holder.bind(getItem(position));
+        if (loginStatus)
+            userReservationCheck(loginId);
+
+        final AppCompatTextView seatNumber;
+        final int seatNum = seatDto.get(position).getSeatNum();
+        final String userId = seatDto.get(position).getUsedId();
+        final boolean seatCheck = seatDto.get(position).isSeatCheck();
+
+        holder.seatNumber.setText("" + seatNum);
+
+        if (!seatCheck)
+            holder.seatNumber.setBackgroundResource(R.drawable.available_seat);
+
+        if (seatCheck) {
+            holder.seatNumber.setBackgroundResource(R.drawable.notavailable_seat);
+        } // 이미지
+
+        holder.seatNumber.setOnClickListener(v -> {
+            Log.e("test", Integer.toString(position));
+            if (!seatCheck) { // 선택한 좌석이 비어 있있다면
+
+                if (userDto.isSeatState()) {   // 사용자가 다른 자리에 이미 예약되어 있다면
+
+                    room1.moveDialog(position, floorNum, userDto, seatDto);   // 자리 이동 다이얼로그 호출
+
+                } else
+
+                    room1.reservationDialog(position, floorNum, userDto, seatDto);    // 자리 예약 다이얼로그 호출
+
+            } else {
+
+                if (userDto.getSeatNum() == (position + 1))     // 선택한 자리가 본인 자리라면
+
+                    room1.cancelDialog(seatDto, userDto);                 // 자리 반환 다이얼로그 호출
+
+                else
+
+                    Toast.makeText(context, "이미 예약되어 있는 자리 입니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 
     // 아이템에 해당하는 뷰 홀더
     static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView seatNumber;
+        ProgressBar pb;
 
-        // 테스트용 텍스트뷰
-        private final AppCompatTextView seatNumber;
+//         // 테스트용 텍스트뷰
+//        private final AppCompatTextView seatNumber;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             seatNumber = itemView.findViewById(R.id.seatNumber);
+            pb = itemView.findViewById(R.id.pb);
         }
 
         private void bind(String word) {
             seatNumber.setText(word);
-            seatNumber.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    System.out.println("좌석 클릭");
-
-                }
-            });
+            seatNumber.setOnClickListener(view -> System.out.println("좌석 클릭"));
         }
+    }
+    @Override
+    public int getItemCount() {
+        return this.seatDto.size();
     }
 
     // ListAdapter DiffUtil
@@ -75,6 +134,24 @@ public class SeatAdapter extends ListAdapter<String, SeatAdapter.MyViewHolder> {
         public boolean areContentsTheSame(@NonNull String oldItem, @NonNull String newItem) {
             return oldItem.equals(newItem);
         }
+    }
+    public synchronized void userReservationCheck(String id) {
+        Log.d("DBUG", "check");
+
+        Query query = databaseReference.child("User").child(id);
+        query.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userDto = dataSnapshot.getValue(UserDTO.class);
+                Log.d("DBUG", "success");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("DBUG", "cancelled");
+            }
+        });
     }
 }
 

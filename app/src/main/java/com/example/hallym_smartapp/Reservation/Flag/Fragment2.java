0be0480beede927087c8ffb4ad1825e40b134855
MminJJ <alsejd107@naver.com>
2022-11-17 23:17:14 +0900

    fragment2 item gridlayout

--- app/src/main/java/com/example/hallym_smartapp/Reservation/Flag/Fragment2.java
+++ app/src/main/java/com/example/hallym_smartapp/Reservation/Flag/Fragment2.java
@@ -8,10 +8,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hallym_smartapp.R;
+import com.example.hallym_smartapp.Reservation.Activity.CountAdapter;
 import com.example.hallym_smartapp.Reservation.Function.SeatAdapter;
+import com.example.hallym_smartapp.Reservation.Function.SeatDto;
 
 import java.util.ArrayList;
 
@@ -21,6 +24,8 @@ public class Fragment2 extends Fragment {
     RecyclerView recyclerView;
     // 리사클러뷰 어댑터 추가
     SeatAdapter seatAdapter;
+
+    GridLayoutManager layout;
 
     public Fragment2(){
     }
@@ -37,7 +42,10 @@ public class Fragment2 extends Fragment {
     // 리사이클러뷰 초기화
     private void initRecyclerView() {
         recyclerView = requireView().findViewById(R.id.recyclerView);
+        layout = new GridLayoutManager(getActivity(), 4);
         seatAdapter = new SeatAdapter(new SeatAdapter.SeatDiffUtil());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layout);
         recyclerView.setAdapter(seatAdapter);
 
         // 테스트용 데이터 추가

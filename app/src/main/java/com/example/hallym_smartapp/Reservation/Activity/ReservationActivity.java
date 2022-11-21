f8b9a20a2d6ee69c0025294777e8301c1428dade
leechaeeyoung <zs1397@naver.com>
2022-11-21 12:41:14 +0900

    mypage, myseat 수정

--- app/src/main/java/com/example/hallym_smartapp/Reservation/Activity/ReservationActivity.java
+++ app/src/main/java/com/example/hallym_smartapp/Reservation/Activity/ReservationActivity.java
@@ -32,60 +32,71 @@ public class ReservationActivity extends AppCompatActivity {
     Intent intent;
     LinearLayout list_3floor;
     TextView thirdSeatNum;
+    Button bt_tab2;
 
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
-
-    // 층수마다 플래그
-    private final int MyPageFrag = 1;
-    private final int FRAGMENT2 = 2;
-    private final int FRAGMENT3 = 3;
-    private final int FRAGMENT4 = 4;
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.reservation_main);
+        super.onCreate(savedInstanceState);
 
-        setTitle("좌석 현황");
-
-
-        list_3floor = findViewById(R.id.list_3floor);
-        nowCnt();
-
-        findViewById(R.id.bt_tab2).setOnClickListener(v -> callFragment(FRAGMENT2));
-
+        bt_tab2=findViewById(R.id.bt_tab2);
+        bt_tab2.setOnClickListener(v -> {
+            intent = new Intent(getApplicationContext(), ReserveDialog.class);
+            startActivity(intent);
+        });
     }
 
-
-    private void callFragment(int fragment_no) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-        switch (fragment_no) {
-            case 1:
-                MyPageFrag mypage = new MyPageFrag();
-                transaction.replace(R.id.fragment_container, mypage);
-                transaction.commit();
-                break;
-            case 2:
-                Fragment2 frag2 = new Fragment2();
-                transaction.replace(R.id.fragment_container, frag2);
-                transaction.commit();
-                break;
-            case 3:
-                Fragment3 frag3 = new Fragment3();
-                transaction.replace(R.id.fragment_container, frag3);
-                transaction.commit();
-                break;
-            case 4:
-                Fragment4 frag4 = new Fragment4();
-                transaction.replace(R.id.fragment_container, frag4);
-                transaction.commit();
-                break;
-        }
-    }
+//    // 층수마다 플래그
+//    private final int MyPageFrag = 1;
+//    private final int FRAGMENT2 = 2;
+//    private final int FRAGMENT3 = 3;
+//    private final int FRAGMENT4 = 4;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.reservation_main);
+//
+//        setTitle("좌석 현황");
+//
+//
+//        list_3floor = findViewById(R.id.list_3floor);
+//        nowCnt();
+//
+//        findViewById(R.id.bt_tab2).setOnClickListener(v -> callFragment(FRAGMENT2));
+//
+//    }
+//
+//
+//    private void callFragment(int fragment_no) {
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//
+//        switch (fragment_no) {
+//            case 1:
+//                MyPageFrag mypage = new MyPageFrag();
+//                transaction.replace(R.id.fragment_container, mypage);
+//                transaction.commit();
+//                break;
+//            case 2:
+//                Fragment2 frag2 = new Fragment2();
+//                transaction.replace(R.id.fragment_container, frag2);
+//                transaction.commit();
+//                break;
+//            case 3:
+//                Fragment3 frag3 = new Fragment3();
+//                transaction.replace(R.id.fragment_container, frag3);
+//                transaction.commit();
+//                break;
+//            case 4:
+//                Fragment4 frag4 = new Fragment4();
+//                transaction.replace(R.id.fragment_container, frag4);
+//                transaction.commit();
+//                break;
+//        }
+//    }
 
     // 현재 좌석 수를 나타네는 메소드
     public void nowCnt() {
